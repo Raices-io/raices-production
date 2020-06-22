76c1fe276bce5d41a55bd3007208a12a4696ef54
scottie-schneider <scott@laclave.co>
2020-06-22 16:35:54 -0500

    think it's working

--- components/Search/Algolia/Range.js
+++ components/Search/Algolia/Range.js
@@ -13,58 +13,69 @@ const StyledSlider = styled.div`
 `;
 
 class Range extends Component {
-  state = { currentValues: { min: this.props.min, max: this.props.max } };
-
+  state = {
+    currentValues: { min: this.props.min, max: this.props.max },
+    min: 0,
+    max: 1000000000,
+  };
   componentDidUpdate(prevProps) {
     if (
       this.props.canRefine &&
       (prevProps.currentRefinement.min !== this.props.currentRefinement.min ||
         prevProps.currentRefinement.max !== this.props.currentRefinement.max)
     ) {
-      console.log("setting state");
       this.setState({
         currentValues: {
           min: this.props.currentRefinement.min,
           max: this.props.currentRefinement.max,
         },
       });
-      console.log(this.state.currentValues);
     }
   }
 
   onValuesUpdated = (sliderState) => {
-    console.log("values updated!");
-    console.log(sliderState);
     this.setState({
-      currentValues: { min: sliderState.values[0], max: sliderState.values[1] },
+      currentValues: {
+        min: parseInt(sliderState.values[0]),
+        max: parseInt(sliderState.values[1]),
+      },
     });
   };
 
   onChange = (sliderState) => {
+    console.log("slider state");
+    console.log(sliderState);
     if (
       this.props.currentRefinement.min !== sliderState.values[0] ||
       this.props.currentRefinement.max !== sliderState.values[1]
     ) {
-      this.props.refine({
-        min: sliderState[0],
-        max: sliderState[1],
-      });
+      if (parseInt(sliderState[0]) == 0) {
+        this.props.refine({
+          min: this.props.min,
+          max: parseInt(sliderState[1]),
+        });
+      } else {
+        this.props.refine({
+          min: parseInt(sliderState[0]),
+          max: parseInt(sliderState[1]),
+        });
+      }
     }
   };
 
   render() {
     const { min, max, currentRefinement } = this.props;
     const { currentValues } = this.state;
-    return min !== max ? (
+    return this.state.min !== this.state.max ? (
       <StyledSlider>
         {/* Styled Components Slider here */}
         <Nouislider
-          min={min}
           onChange={this.onChange}
           onValuesUpdated={this.onValuesUpdated}
-          range={{ min: min, max: max }}
-          start={[min, max]}
+          range={{ min: 0, max: 1000000000 }}
+          start={[0, 1000000000]}
           connect
+          tooltips={[true, true]}
           step={1000}
         />
         <div className="rheostat-values">
@@ -74,7 +85,7 @@ class Range extends Component {
               thousandsGroupStyle="thousand"
               displayType="text"
               prefix={"$"}
-              value={currentRefinement.min}
+              value={0}
             />
           </div>
           <div>
@@ -83,7 +94,7 @@ class Range extends Component {
               thousandsGroupStyle="thousand"
               displayType="text"
               prefix={"$"}
-              value={currentRefinement.max}
+              value={10000000}
             />
           </div>
         </div>

4677a94bee4b72e741d47e8ccde8b96d4522242f
scottie-schneider <scott@laclave.co>
2020-06-23 15:37:54 -0500

    next styles

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { ProvideAuth } from "../util/auth.js";
 import "../styles/imageUploader.css";
 // Div100vh helps solve the height issues I faced when developing across devices and screen sizes
 import Div100vh from "react-div-100vh";
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 // Global style to prevent page scrolling while modal open
 const GlobalStyle = createGlobalStyle`
@@ -15,15 +15,28 @@ const GlobalStyle = createGlobalStyle`
     display: none;
   }
 `;
+const theme = {
+  color100: "#EBF4FF",
+  color200: "#C3DAFE",
+  color300: "#A3BFFA",
+  color400: "#7F9CF5",
+  color500: "#667EEA",
+  color600: "#5A67D8",
+  color700: "#4C51BF",
+  color800: "#434190",
+  color900: "#3C366B",
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ProvideAuth>
-      <GlobalStyle />
-      <Div100vh>
-        <Component {...pageProps} />
-      </Div100vh>
-    </ProvideAuth>
+    <ThemeProvider theme={theme}>
+      <ProvideAuth>
+        <GlobalStyle />
+        <Div100vh>
+          <Component {...pageProps} />
+        </Div100vh>
+      </ProvideAuth>
+    </ThemeProvider>
   );
 }
 

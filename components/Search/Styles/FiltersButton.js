4d9d6f2f8919799ce69ffd5fa3e722f96b809f6c
scottie-schneider <scott@laclave.co>
2020-06-24 14:00:02 -0500

    search functionality... done

--- components/Search/Styles/FiltersButton.js
+++ components/Search/Styles/FiltersButton.js
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 const FiltersButton = styled.button`
   background: white;
-  border: 1px solid ${(props) => props.theme.color500};
+  border: 2px solid ${(props) => props.theme.color500};
   color: ${(props) => props.theme.color600};
   padding: 0.5rem;
   height: 100;
   border-radius: 5px;
+  font-weight: 600;
 `;
 
 export default FiltersButton;

6a6b3a3e122242149a36a792e0c78902c4496424
scottie-schneider <scott@laclave.co>
2020-06-24 10:33:25 -0500

    responsive grid

--- pages/explorar.js
+++ pages/explorar.js
@@ -77,7 +77,7 @@ const Search = (props) => {
               toggleModal();
             }}
           >
-            Filters
+            Filtros
           </FiltersButton>{" "}
         </div>
         <div className="header">

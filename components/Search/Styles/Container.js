6f42880f3adcdb847cef40887c3ac8349f1b5faf
scottie-schneider <scott@laclave.co>
2020-06-25 14:43:46 -0500

    fixed image issue

--- components/Search/Styles/Container.js
+++ components/Search/Styles/Container.js
@@ -6,12 +6,15 @@ const Container = styled.div`
   bottom: 0;
   left: 0;
   width: 100%;
-  height: 100%;
+  height: calc(100% - 48px);
+  @media (min-width: 768px) {
+    height: 100%;
+    padding-top: 80px;
+  }
   display: flex;
   flex-shrink: 0;
   flex-direction: column;
   flex-grow: 1;
-  padding-bottom: 48px;
   overflow: hidden;
   @media (min-width: 768px) {
     margin-bottom: 0;

f85de98568aa5c00d59f92cdcb7e91b6a3c60f77
SebastianGarces <gsgarces1@gmail.com>
2020-06-26 11:45:22 -0400

    landpage redesign

--- components/LandingPage/RoomType.js
+++ components/LandingPage/RoomType.js
@@ -1,23 +1,30 @@
 import { connectRefinementList } from 'react-instantsearch-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const RoomType = connectRefinementList(({ items, refine, currentRefinement }) => {
+const RoomType = connectRefinementList(({ items, refine, currentRefinement, input }) => {
 	const sortedItems = items.sort((i1, i2) => i1.label.localeCompare(i2.label));
 
-	const hitComponents = sortedItems.map(item => {
+	const hitComponents = sortedItems.map((item, index) => {
 		const selectedClassName = item.isRefined ? ' ais-refinement-list--item__active' : '';
-		const itemClassName = `ais-refinement-list--item col-sm-3 ${selectedClassName}`;
+		const itemClassName = `ais-refinement-list--item ${selectedClassName}`;
 
 		return (
-			<div className={itemClassName} key={item.label}>
-				<StyledTab selected={currentRefinement.includes(item.label)}>
+			<div
+				className={`${itemClassName}`}
+				key={item.label}
+				style={{ marginBottom: '5px', cursor: 'pointer' }}>
+				<StyledTab
+					selected={currentRefinement.includes(item.label)}
+					className={`ais-refinement-list--label ${index === 0 ? 'left' : 'right'} ${
+						sortedItems.length === 1 ? 'single' : ''
+					}`}>
 					<TabButton
 						className="ais-refinement-list--label"
 						onClick={e => {
 							e.preventDefault();
 							refine(item.value);
 						}}>
-						<span>{item.label == 'rent' ? 'Alquilar' : 'Comprar'} </span>
+						<p>{item.label == 'rent' ? 'Alquilar' : 'Comprar'} </p>
 					</TabButton>
 				</StyledTab>
 			</div>
@@ -26,38 +33,52 @@ const RoomType = connectRefinementList(({ items, refine, currentRefinement }) =>
 
 	return (
 		<div className="row aisdemo-filter">
-			<StyledTabs id="room_types col-sm-3">{hitComponents}</StyledTabs>
+			<StyledTabs id="room_types" input={input}>
+				{hitComponents}
+			</StyledTabs>
 		</div>
 	);
 });
 
 const StyledTab = styled.div`
-	border-radius: 5px;
-	color: rgb(255, 255, 255);
-	background-color: ${props => (props.selected ? 'white' : 'transparent')};
-	color: ${props => (props.selected ? '#5A67D8' : 'white')};
-	@media (min-width: 640px) {
-		&:hover {
-			background: white;
-			color: #5a67d8;
-		}
+	color: rgba(255, 255, 255, 1);
+	background-color: ${props => (props.selected ? '#f1f1f1' : 'transparent')};
+	color: ${props => (props.selected ? '#5A67D8' : '#f1f1f1')};
+	padding: 0.25rem 1rem;
+
+	&.left {
+		border-radius: 5px 0 0 5px;
 	}
-	height: 100%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	padding: 1rem;
+
+	&.right {
+		border-radius: 0 5px 5px 0;
+	}
+
+	&.single {
+		border-radius: 5px;
+	}
+
+	&:hover {
+		background: #f1f1f1;
+		color: #5a67d8;
+	}
 `;
 
 const StyledTabs = styled.div`
-	background-color: rgba(59, 65, 68, 0.6);
+	background-color: rgba(0, 0, 0, 0.4);
 	display: flex;
-	height: 50px;
-	width: 50%;
-	margin: 0 auto;
+	width: fit-content;
 	border-radius: 5px;
 	justify-content: space-between;
-	max-width: 200px;
+
+	${props =>
+		props.input &&
+		css`
+			@media (max-width: 768px) {
+				position: absolute;
+				margin-top: 62px;
+			}
+		`}
 `;
 
 const TabButton = styled.div`

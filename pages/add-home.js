cece1b5f78be03e4bafba79c6bd15832214c8d38
scottie-schneider <scott@laclave.co>
2020-07-10 12:38:45 -0500

    fixing layout issues

--- pages/add-home.js
+++ pages/add-home.js
@@ -4,11 +4,10 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../util/auth';
 import { useRouter } from 'next/router';
-import TopNav from '../components/Navigation/TopNav';
-import BottomNav from '../components/Navigation/BottomNav';
 import { firebase } from '../util/firebase';
 import PendingHomeForm from '../components/Forms/PendingHomeForm';
 import LoadingPage from '../components/LoadingSpinner/LoadingPage';
+import Layout from '../components/Layout';
 const Add = () => {
 	// ****
 	// AUTH
@@ -59,19 +58,11 @@ const Add = () => {
 		}
 	}, [user]);
 	return (
-		<div className=" relative flex flex-col w-screen h-full flex-grow bg-white overflow-y-scroll antialiased">
-			<div className="z-40 hidden md:block px-12 mt-12">
-				<TopNav fixed />
-			</div>
-			<div className="sm:py-6 pb-12 flex flex-col overflow-y-scroll h-full flex-grow mx-0 flex-grow mt-8">
-				{/* Container for homes in progress vs create a new one */}
-				{loading && <LoadingPage />}
-				{!loading && <PendingHomeForm pendingHomes={pendingHomes} />}
-			</div>
-			<div className="flex w-full md:hidden">
-				<BottomNav />
-			</div>
-		</div>
+		<Layout>
+			{/* Container for homes in progress vs create a new one */}
+			{loading && <LoadingPage />}
+			{!loading && <PendingHomeForm pendingHomes={pendingHomes} />}
+		</Layout>
 	);
 };
 export default Add;

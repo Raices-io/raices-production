e65137f0e2e8c501cf60831c1bd5478f0d61d882
camilomontoyau <camilomontoya2@gmail.com>
2020-08-03 16:31:15 -0500

    export imageResizing

--- packages/backend/functions/triggers/index.js
+++ packages/backend/functions/triggers/index.js
@@ -1,5 +1,6 @@
 const pending = require('./pending');
 const signups = require('./signups');
 const agents = require('./agents');
+const imageResizing = require('./image-resizing');
 
-module.exports = { pending, signups, agents };
+module.exports = { pending, signups, agents, imageResizing };

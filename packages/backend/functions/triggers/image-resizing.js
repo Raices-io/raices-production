e74c7c79c38f8a1bec6cd178e562dbd5004cebb8
camilomontoyau <camilomontoya2@gmail.com>
2020-08-03 16:29:59 -0500

    image-resizinf format

--- packages/backend/functions/triggers/image-resizing.js
+++ packages/backend/functions/triggers/image-resizing.js
@@ -10,84 +10,83 @@ const imageminMozjpeg = require('imagemin-mozjpeg');
 
 const storage = new Storage();
 
-exports.imageResizing = async object => {
-    const fileBucket = object.bucket;
-    const filePath = object.name;
-    const contentType = object.contentType;
-  
-    if (fileBucket && filePath && contentType) {
-        // Exit if this is triggered on a file that is not an image.
-        if (!contentType.startsWith('image/')) {
-            console.log('This is not an image.');
-            return true;
-        }
-        
-        // Get the file name.
-        const extendName = extname(filePath);
-        const fileName = basename(filePath, extendName);
-        const fileFullName = `${fileName}${extendName}`;
-        console.log('=======this=======',JSON.stringify({extendName, fileName, fileFullName, filePath}))
-        if (fileName.includes('_thumb_')) {
-            console.log('Already a Thumbnail.');
-            return true;
-        }
+module.exports = {
+	imageResizing: async object => {
+		const fileBucket = object.bucket;
+		const filePath = object.name;
+		const contentType = object.contentType;
 
-        // Download file from bucket.
-        const bucket = storage.bucket(fileBucket);
-        const file = bucket.file(filePath);
-        const [data] = await file.getMetadata();
-        if (data.metadata && data.metadata.optimized) {
-            console.log('Image has been already optimized');
-            return true;
-        }
+		if (fileBucket && filePath && contentType) {
+			// Exit if this is triggered on a file that is not an image.
+			if (!contentType.startsWith('image/')) {
+				console.log('This is not an image.');
+				return true;
+			}
 
-        const workingDir = join(tmpdir(), 'thumbs');
-        const destination = join(workingDir, fileFullName);
-        await fs.ensureDir(workingDir);
-        await file.download({ destination });
-        const bucketDir = dirname(filePath);
+			// Get the file name.
+			const extendName = extname(filePath);
+			const fileName = basename(filePath, extendName);
+			const fileFullName = `${fileName}${extendName}`;
+			console.log(
+				'=======this=======',
+				JSON.stringify({ extendName, fileName, fileFullName, filePath }),
+			);
+			if (fileName.includes('_thumb_')) {
+				console.log('Already a Thumbnail.');
+				return true;
+			}
 
-        const sizes = [480, 640, 1200];
-        const resizesPromises = sizes.map((size) => {
-        const thumbName = `${fileName}_thumb_${size}${extendName}`;
-        const thumbPath = join(workingDir, thumbName);
-        return sharp(destination)
-            .resize(size)
-            .toFile(thumbPath);
-        });
-        await Promise.all(resizesPromises);
-        console.log('generate 3 images per devices, done!');
+			// Download file from bucket.
+			const bucket = storage.bucket(fileBucket);
+			const file = bucket.file(filePath);
+			const [data] = await file.getMetadata();
+			if (data.metadata && data.metadata.optimized) {
+				console.log('Image has been already optimized');
+				return true;
+			}
 
-        await imagemin([`${workingDir}/*.{jpg,png}`], {
-            destination: workingDir,
-            plugins: [
-              imageminPngquant({quality: [0.6, 0.6]}),
-              imageminMozjpeg({quality: 60}),
-            ]
-        });
-        console.log('optimize jpg and png, done!');
+			const workingDir = join(tmpdir(), 'thumbs');
+			const destination = join(workingDir, fileFullName);
+			await fs.ensureDir(workingDir);
+			await file.download({ destination });
+			const bucketDir = dirname(filePath);
 
-        const files = await fs.readdir(workingDir);
-        console.log(files);
+			const sizes = [480, 640, 1200];
+			const resizesPromises = sizes.map(size => {
+				const thumbName = `${fileName}_thumb_${size}${extendName}`;
+				const thumbPath = join(workingDir, thumbName);
+				return sharp(destination).resize(size).toFile(thumbPath);
+			});
+			await Promise.all(resizesPromises);
+			console.log('generate 3 images per devices, done!');
 
-        const uploadPromises = files.map(file => {
-            const path = join(workingDir, file);
-            return bucket.upload(path, {
-                destination: join(bucketDir, basename(file)),
-                metadata: {
-                    metadata: {
-                        optimized: true
-                    }
-                }
-            });
-        });
-        await Promise.all(uploadPromises);
-        console.log('upload images, done!');
+			await imagemin([`${workingDir}/*.{jpg,png}`], {
+				destination: workingDir,
+				plugins: [imageminPngquant({ quality: [0.6, 0.6] }), imageminMozjpeg({ quality: 60 })],
+			});
+			console.log('optimize jpg and png, done!');
 
-        return fs.remove(workingDir);
+			const files = await fs.readdir(workingDir);
+			console.log(files);
 
-    } else {
-      console.log('incomplete data');
-      return false;
-    }
+			const uploadPromises = files.map(file => {
+				const path = join(workingDir, file);
+				return bucket.upload(path, {
+					destination: join(bucketDir, basename(file)),
+					metadata: {
+						metadata: {
+							optimized: true,
+						},
+					},
+				});
+			});
+			await Promise.all(uploadPromises);
+			console.log('upload images, done!');
+
+			return fs.remove(workingDir);
+		} else {
+			console.log('incomplete data');
+			return false;
+		}
+	},
 };
